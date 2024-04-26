51164dda5c61d29ac982b3120f23638d1f78f91c
Matheus Henrique Dias do Nascimento <matheus.mhddn@gmail.com>
2024-04-26 15:57:42 -0300

    create `Ingredient` component

---
+++ src/components/Ingredient/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.span`
+  background: ${({ theme }) => theme.COLORS.BLUE_300};
+
+  font-size: 1.4rem;
+
+  padding: 0.4rem 0.8rem;
+  border-radius: 0.5rem;
+`;
