e0b2c66899c32156f412a8a9863aed2b7c86bb18
Matheus Henrique Dias do Nascimento <matheus.mhddn@gmail.com>
2024-04-29 13:16:12 -0300

    create `Create` page and set route

---
+++ src/pages/Create/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+
+export function Create() {
+  return (
+    <Container>
+      <Header />
+      <Footer />
+    </Container>
+  );
+}
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import { Home } from "../pages/Home";
 import { Details } from "../pages/Details";
+import { Create } from "../pages/Create";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/details" element={<Details />} />
+      <Route path="/create" element={<Create />} />
     </Routes>
   );
 }
