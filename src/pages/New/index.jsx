4063c650927e61c3448d746909a2c6a00ac5f44b
Matheus Henrique Dias do Nascimento <matheus.mhddn@gmail.com>
2024-06-03 22:18:05 -0300

    udpate `New` page

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { Container, Form } from "./styles";
 
 import { PiCaretLeftBold, PiUploadSimple } from "react-icons/pi";
-import { FaChevronDown } from "react-icons/fa";
 
 import { Button } from "../../components/Button";
 import { Footer } from "../../components/Footer";
@@ -12,8 +11,14 @@ import { IngredientTag } from "../../components/IngredientTag";
 import { Input } from "../../components/Input";
 import { Select } from "../../components/Select";
 import { Textarea } from "../../components/Textarea";
+import { api } from "../../services/api";
 
 export function New() {
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
   const [ingredients, setIngredients] = useState([]);
   const [newIngredient, setNewIngredient] = useState("");
 
@@ -28,6 +33,32 @@ export function New() {
     setIngredients(prevState =>
       prevState.filter(ingredient => ingredient !== deleted)
     );
+  }
+
+  async function handleSubmit() {
+    if (!name || !category || !price || !description) {
+      return alert("Preencha todos os campos corretamente!");
+    }
+
+    await api
+      .post("/dishes", {
+        image,
+        name,
+        category,
+        price,
+        description,
+        ingredients,
+      })
+      .then(alert("Prato criado com sucesso"))
+      .catch(error => {
+        if (error.response.message) {
+          alert(error.response.message);
+        } else {
+          alert(error);
+        }
+      });
+
+    navigate("/");
   }
   return (
     <Container>
@@ -47,6 +78,7 @@ export function New() {
                 label="Selecionar imagem"
                 type="file"
                 id="image"
+                onChange={e => setImage(e.target.value)}
               />
             </div>
             <Input
@@ -54,12 +86,17 @@ export function New() {
               type="text"
               id="name"
               placeholder="Ex.: Salada Ceasar"
+              onChange={e => setName(e.target.value)}
             />
-            <Select className="category" label="Categoria">
-              <option>Refeição</option>
-              <option>Sobremesa</option>
-              <option>Bebida</option>
-              <FaChevronDown />
+            <Select
+              className="category"
+              label="Categoria"
+              onChange={e => setCategory(e.target.value)}
+            >
+              <option value="default">Escolha uma categoria</option>
+              <option value="refeicao">Refeição</option>
+              <option value="sobremesa">Sobremesa</option>
+              <option value="bebida">Bebida</option>
             </Select>
           </section>
 
@@ -67,14 +104,13 @@ export function New() {
             <div className="input-wrapper">
               <label id="ingredients_label">Ingredientes</label>
               <ul>
-                {ingredients &&
-                  ingredients.map((ingredient, index) => (
-                    <IngredientTag
-                      key={String(index)}
-                      value={ingredient}
-                      onClick={() => handleRemoveIngredient(ingredient)}
-                    />
-                  ))}
+                {ingredients.map((ingredient, index) => (
+                  <IngredientTag
+                    key={String(index)}
+                    value={ingredient}
+                    onClick={() => handleRemoveIngredient(ingredient)}
+                  />
+                ))}
                 <IngredientTag
                   $isnew={true}
                   onChange={e => setNewIngredient(e.target.value)}
@@ -84,15 +120,25 @@ export function New() {
               </ul>
             </div>
             <div className="input-wrapper">
-              <Input label="Preço" type="number" placeholder="R$ 00,00" />
+              <Input
+                label="Preço"
+                type="number"
+                placeholder="R$ 00,00"
+                onChange={e => setPrice(e.target.value)}
+              />
             </div>
           </section>
 
           <Textarea
             label="Descrição"
             placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+            onChange={e => setDescription(e.target.value)}
           />
-          <Button id="submit_button" title="Salvar alterações" />
+          <Button
+            id="submit_button"
+            title="Salvar alterações"
+            onClick={handleSubmit}
+          />
         </Form>
       </main>
       <Footer />
